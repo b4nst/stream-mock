aed6099c80f137364ed9bcbd0b561bfa4457b520
Bastien Arata <banst@Bastiens-MacBook-Pro.local>
2018-03-10 22:54:48 +0100

    :sparkles: add WritableMock
    add WritableMock

---
+++ src/main/writable-mock.js
@@ -0,0 +1,33 @@
+import { Writable } from 'stream';
+
+export default class WritableMock extends Writable {
+  constructor(options) {
+    super(options);
+    this.data = [];
+  }
+
+  _write(chunk, encoding, callback) {
+    this.data.push(chunk);
+    callback();
+  }
+
+  _writev(chunks, encoding, callback) {
+    const datas = chunks.map(c => c.chunk);
+    this.data = this.data.concat(datas);
+    callback();
+  }
+
+  _final(callback) {
+    if (!this._writableState.objectMode) {
+      const length = this.data.reduce((acc, curr) => acc += curr.length, 0);
+      const buf = Buffer.alloc(length);
+      let offset = 0;
+      for (const d of this.data) {
+        d.copy(buf, offset);
+        offset += d.length;
+      }
+      this.data = buf;
+    }
+    callback();
+  }
+}
