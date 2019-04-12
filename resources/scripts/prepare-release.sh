52a135e2c605003ad5e17338644dd4ec8c539ffd
Bastien Arata <bastien.arata@d2si.io>
2019-04-12 17:07:55 +0200

    :construction_worker: bump major

--- resources/scripts/prepare-release.sh
+++ resources/scripts/prepare-release.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+yarn run build
+yarn config set version-tag-prefix "v"
+
 version=$(echo $TRAVIS_COMMIT_MESSAGE | sed 's/:construction_worker: bump//' | sed -e 's/  *$//' | sed -e 's/ //g')
 if [ -z "$version" ]
 then
     version=patch
 fi
-
-yarn config set version-tag-prefix "v"
 
 case $version in
 major)
@@ -23,5 +24,14 @@ patch)
     exit 1
     ;;
 esac
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git add .
+git commit -m ':construction_worker: release ready'
+
+
+git remote add origin-ci https://${GH_TOKEN}@github.com/BastienAr/stream-mock.git > /dev/null 2>&1
+git push --tags --quiet --set-upstream origin-ci master 
 
 exit 0
